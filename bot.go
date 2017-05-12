b648289fa5b4433161fa78e21fb43259b378abdc
Dean Bassett <deanvelopermn@gmail.com>
2017-05-11 20:28:49 -0500

    Go fmt, added comments, new timing system for tweeting

--- bot.go
+++ bot.go
@@ -1,60 +1,60 @@
 package xkcdnews
 
 import (
-    "github.com/dghubble/go-twitter/twitter"
-    "github.com/dghubble/oauth1"
-    "io/ioutil"
-    "log"
-    "os"
-    "path"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 type Bot struct {
-    done   chan bool
-    myId   int64
-    client *twitter.Client
-    log    *log.Logger
+	done   chan bool
+	myId   int64
+	client *twitter.Client
+	log    *log.Logger
 }
 
 func New(log *log.Logger) *Bot {
-    return &Bot{make(chan bool), 0, nil, log}
+	return &Bot{make(chan bool), 0, nil, log}
 }
 
 func (b *Bot) Start() {
-    b.log.Println("Starting XKCDNews!")
-    consumer, err := ioutil.ReadFile(path.Join(os.Getenv("HOME"), "botfiles", "xkcdnews", "CONSUMER_SECRET"))
-    if err != nil {
-        b.log.Println("Error loading consumer secret: ", err)
-        return
-    }
-    access, err := ioutil.ReadFile(path.Join(os.Getenv("HOME"), "botfiles", "xkcdnews", "ACCESS_SECRET"))
-    if err != nil {
-        b.log.Println("Error loading access secret: ", err)
-        return
-    }
-    b.log.Println("API Keys loaded")
+	b.log.Println("Starting XKCDNews!")
+	consumer, err := ioutil.ReadFile(path.Join(os.Getenv("HOME"), "botfiles", "xkcdnews", "CONSUMER_SECRET"))
+	if err != nil {
+		b.log.Println("Error loading consumer secret: ", err)
+		return
+	}
+	access, err := ioutil.ReadFile(path.Join(os.Getenv("HOME"), "botfiles", "xkcdnews", "ACCESS_SECRET"))
+	if err != nil {
+		b.log.Println("Error loading access secret: ", err)
+		return
+	}
+	b.log.Println("API Keys loaded")
 
-    config := oauth1.NewConfig("iscYDnN0k5thQJUlGi0BLkQDl", string(consumer))
-    token := oauth1.NewToken("862666402515255297-rFZpHgQX21sdMUXHKUhnVsN43qUNsXx", string(access))
-    httpClient := config.Client(oauth1.NoContext, token)
+	config := oauth1.NewConfig("iscYDnN0k5thQJUlGi0BLkQDl", string(consumer))
+	token := oauth1.NewToken("862666402515255297-rFZpHgQX21sdMUXHKUhnVsN43qUNsXx", string(access))
+	httpClient := config.Client(oauth1.NoContext, token)
 
-    b.client = twitter.NewClient(httpClient)
+	b.client = twitter.NewClient(httpClient)
 
-    verify := &twitter.AccountVerifyParams{
-        SkipStatus:   twitter.Bool(true),
-        IncludeEmail: twitter.Bool(true),
-    }
-    user, _, err := b.client.Accounts.VerifyCredentials(verify)
-    if err != nil {
-        b.log.Println("Error verifying credentials:", err)
-        return
-    }
-    b.log.Println("Logged into ", user.ScreenName)
-    b.myId = user.ID
+	verify := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, _, err := b.client.Accounts.VerifyCredentials(verify)
+	if err != nil {
+		b.log.Println("Error verifying credentials:", err)
+		return
+	}
+	b.log.Println("Logged into ", user.ScreenName)
+	b.myId = user.ID
 
-    go b.startTweeting()
+	go b.startTweeting()
 }
 
 func (b *Bot) Stop() {
-    b.done <- true
+	b.done <- true
 }
--- tweet.go
+++ tweet.go
@@ -1,79 +1,101 @@
 package xkcdnews
 
 import (
-    "github.com/dghubble/go-twitter/twitter"
-    "time"
+	"github.com/dghubble/go-twitter/twitter"
+	"time"
 )
 
+// Starts the timer to continuously tweet
 func (b *Bot) startTweeting() {
-    b.postNewTweet()
-
-    ticker := time.NewTicker(30 * time.Minute)
-    for {
-        select {
-        case <-ticker.C:
-            b.postNewTweet()
-        case <-b.done:
-            ticker.Stop()
-            return
-        }
-    }
+	for {
+		tryAgain := time.After(b.postNewTweet())
+		select {
+		case <-tryAgain:
+			b.tryPostNewTweet()
+		case <-b.done:
+			tryAgain = nil
+			return
+		}
+	}
 }
 
-func (b *Bot) postNewTweet() {
-    utlParams := &twitter.UserTimelineParams{
-        UserID: b.myId,
-        Count:  1,
-    }
-    lastTweet, _, err := b.client.Timelines.UserTimeline(utlParams)
-    if err != nil {
-        b.log.Println("Error fetching last tweet:", err)
-        return
-    }
-    lastTime, err := time.Parse(time.RubyDate, lastTweet[0].CreatedAt)
-    if err != nil {
-        b.log.Println("Error formatting time:", err)
-        return
-    }
+// Makes sure that there hasn't been a new tweet
+// in the past hour before tweeting.
+//
+// Return: The duration until this should be called again
+func (b *Bot) tryPostNewTweet() time.Duration {
 
-    if lastTime.Add(time.Hour).After(time.Now()) {
-        // if it hasn't been an hour, retry later
-        return
-    }
+	// Get our most recent tweet
+	utlParams := &twitter.UserTimelineParams{
+		UserID: b.myId,
+		Count:  1,
+	}
+	lastTweet, _, err := b.client.Timelines.UserTimeline(utlParams)
+	if err != nil {
+		b.log.Println("Error fetching last tweet:", err)
+		return 5 * time.Minute
+	}
 
-    tlParams := &twitter.HomeTimelineParams{
-        Count:           20,
-        TrimUser:        twitter.Bool(true),
-        ExcludeReplies:  twitter.Bool(true),
-        IncludeEntities: twitter.Bool(false),
-    }
-    tweets, _, err := b.client.Timelines.HomeTimeline(tlParams)
-    if err != nil {
-        b.log.Println("Error fetching timeline:", err)
-        return
-    }
+	// Get the time the tweet was posted
+	lastTime, err := time.Parse(time.RubyDate, lastTweet[0].CreatedAt)
+	if err != nil {
+		// A time format error means an error on my part most likely,
+		// so make the duration longer
+		b.log.Println("Error formatting time:", err)
+		return 5 * time.Hour
+	}
 
-    var bestTweet string
-    bestTweetRating := 0
-    for _, tweet := range tweets {
-        newTweet, replaced := Substitute(tweet.Text)
-        if len(newTweet) > 140 {
-            continue
-        }
-        if replaced > bestTweetRating {
-            bestTweet = newTweet
-            bestTweetRating = replaced
-        }
-    }
+	// if it hasn't been an hour since our last tweet, retry when the hour is up
+	if lastTime.Add(time.Hour).After(time.Now()) {
+		return time.Now().Sub(lastTime) + time.Minute
+	}
 
-    if bestTweetRating > 0 {
+	return b.postNewTweet()
+}
 
-        params := &twitter.StatusUpdateParams{
-            TrimUser: twitter.Bool(true),
-        }
-        _,_, err := b.client.Statuses.Update(bestTweet, params)
-        if err != nil {
-            b.log.Println("Error posting tweet:", err)
-        }
-    }
+// Posts a new tweet.
+//
+// Return: The duration until tryPostNewTweet() should be called
+func (b *Bot) postNewTweet() time.Duration {
+	// Get the last 20 posts
+	tlParams := &twitter.HomeTimelineParams{
+		Count:           20,
+		TrimUser:        twitter.Bool(true),
+		ExcludeReplies:  twitter.Bool(true),
+		IncludeEntities: twitter.Bool(false),
+	}
+	tweets, _, err := b.client.Timelines.HomeTimeline(tlParams)
+	if err != nil {
+		b.log.Println("Error fetching timeline:", err)
+		return 5 * time.Minute
+	}
+
+	// Look through the tweets, replace words,
+	// and pick the tweet with the most replaced words
+	var bestTweet string
+	bestTweetRating := 0
+	for _, tweet := range tweets {
+		newTweet, replaced := Substitute(tweet.Text)
+		if len(newTweet) > 140 {
+			continue
+		}
+		if replaced > bestTweetRating {
+			bestTweet = newTweet
+			bestTweetRating = replaced
+		}
+	}
+
+	// Post the tweet!
+	if bestTweetRating > 0 {
+		params := &twitter.StatusUpdateParams{
+			TrimUser: twitter.Bool(true),
+		}
+		_, _, err := b.client.Statuses.Update(bestTweet, params)
+		if err != nil {
+			b.log.Println("Error posting tweet:", err)
+			return 5 * time.Minute
+		}
+		return 1 * time.Hour
+	}
+	return 5 * time.Minute
 }
