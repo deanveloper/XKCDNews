f84d185f3840b30481011d3ff1eeee9fa19d73bd
Dean Bassett <deanvelopermn@gmail.com>
2017-05-12 00:55:40 -0500

    better regex

--- substitution.go
+++ substitution.go
@@ -67,7 +67,7 @@ var subs = [][2]string{
 	{"scientists", "channing tatum and his friends"},
 	{"you won't believe", "i'm really sad about"},
 }
-var URL_PATTERN = regexp.MustCompile(`\b(https?://\w+)\b`)
+var URL_PATTERN = regexp.MustCompile(`/(https?://)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*/?/`)
 
 func Substitute(s string) (string, int) {
 	replaced := 0
